8c197ed0f3269ada376f0c15d059ab45937b2aef
kin16742 <kin16742@naver.com>
2020-04-30 18:42:05 +0900

    file 입력 및 file명 출력

--- voiceSeparator.py
+++ voiceSeparator.py
@@ -1,26 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import *
+
 
 class voiceSeparator(QWidget):
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        fileInputBtn = QPushButton('파일 입력', self)
-        separateBtn = QPushButton('음성 분리하기', self)
-        fileNameLabel = QLabel('파일 이름', self)
+        self.fileInputBtn = QPushButton('파일 입력')
+        self.separateBtn = QPushButton('음성 분리하기')
+        self.fileName = QLabel()
 
         hbox1 = QHBoxLayout()
         hbox1.addStretch(1)
-        hbox1.addWidget(fileNameLabel)
-        hbox1.addStretch(1)
-        hbox1.addWidget(fileInputBtn)
+        hbox1.addWidget(self.fileName)
         hbox1.addStretch(1)
 
         hbox2 = QHBoxLayout()
         hbox2.addStretch(1)
-        hbox2.addWidget(separateBtn)
+        hbox2.addWidget(self.fileInputBtn)
+        hbox2.addStretch(1)
+        hbox2.addWidget(self.separateBtn)
         hbox2.addStretch(1)
 
         vbox = QVBoxLayout()
@@ -29,6 +31,9 @@ class voiceSeparator(QWidget):
         vbox.addStretch(1)
         vbox.addLayout(hbox2)
         vbox.addStretch(1)
+
+        self.fileInputBtn.clicked.connect(self.fileInput)
+        self.separateBtn.clicked.connect(self.separate)
 
         self.setLayout(vbox)
         self.setWindowTitle('Voice Separator')
@@ -42,7 +47,19 @@ class voiceSeparator(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def fileInput(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', './')
+
+        if fname[0]:
+            self.fileName.setText(fname[0])
+        else:
+            QMessageBox.about(self, "warning", "파일을 선택하지 않았습니다.")
+
+    def separate(self):
+        self.resize(100, 100)
+
+
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = voiceSeparator()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = voiceSeparator()
+    sys.exit(app.exec_())
